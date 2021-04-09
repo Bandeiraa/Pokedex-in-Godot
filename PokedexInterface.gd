0ddab5d664d85168fccd98132c84420bf8323f9e
davi bandeira <fcodavibandeira@edu.unifor.br>
2021-04-08 23:34:42 -0300

    Getting the list of all pokemon, and code refactoring

--- PokedexInterface.gd
+++ PokedexInterface.gd
@@ -1,35 +1,40 @@
 extends Control
 
-onready var vertical_container = get_node("VBoxContainer")
+onready var vertical_container = get_node("ScrollContainer/VBoxContainer")
+
+var current_index = 0
+var pokemon_id = 0
 
 func _ready():
-	for i in 808:
+	request_pokemon_list_size()
+	
+func request_pokemon_list_size():
+	var pokemon_total_list_url = HTTPRequest.new()
+	add_child(pokemon_total_list_url)
+	pokemon_total_list_url.connect("request_completed", self, "_get_pokemon_list_size")
+	pokemon_total_list_url.request("https://pokeapi.co/api/v2/pokemon?limit=898&offset=0") 
+	
+func _get_pokemon_list_size(_result, _response_code, _headers, body):
+	var json = JSON.parse(body.get_string_from_utf8())
+	var pokemon_list_size = json.result.results.size()
+	for i in pokemon_list_size:
 		var label = Label.new()
 		vertical_container.add_child(label)
+		
+	_request_current_pokemon_list()
 	
+func _request_current_pokemon_list():
 	var http_request = HTTPRequest.new()
 	add_child(http_request)
-	http_request.connect("request_completed", self, "_request_pokemon_list")
-	http_request.request("https://pokeapi.co/api/v2/pokemon?limit=19&offset=0")
+	http_request.connect("request_completed", self, "_get_current_pokemon_list")
+	http_request.request("https://pokeapi.co/api/v2/pokemon?limit=20&offset=0")
 	
-func _request_pokemon_list(_result, _responde_code, _headers, body):
+func _get_current_pokemon_list(_result, _responde_code, _headers, body):
 	var json = JSON.parse(body.get_string_from_utf8())
-	for pokemon_url in json.result.results:
-		send_pokemon_url(pokemon_url.url)
+	for pokemon_name in json.result.results:
+		get_current_pokemon(pokemon_name.name)
 
-func send_pokemon_url(poke_url):
-	var pokemon_http_request = HTTPRequest.new()
-	add_child(pokemon_http_request)
-	pokemon_http_request.connect("request_completed", self, "_request_pokemon")
-	pokemon_http_request.request(poke_url)
-	
-func _request_pokemon(_result, _response_code, _headers, body):
-	var json = JSON.parse(body.get_string_from_utf8())
-	var pokemon_name = json.result.name
-	var pokemon_index = json.result.id
-	pokemon_info_list(pokemon_name, pokemon_index)
-
-func pokemon_info_list(pkmn_name, pkmn_id):
-	for children in vertical_container.get_children().size():
-		if children + 1 == pkmn_id:
-			vertical_container.get_child(children).text = "Nome: " + pkmn_name + " Id: " + str(pkmn_id)
+func get_current_pokemon(poke_name):
+	pokemon_id += 1
+	current_index += 1
+	vertical_container.get_child(current_index - 1).text = "Id: " + str(pokemon_id) + " Nome: " + poke_name
